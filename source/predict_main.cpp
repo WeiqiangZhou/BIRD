55782658d0e6eddf530b10ace3a3a6020591ba06
Weiqiang Zhou <kenzhou86@gmail.com>
2015-04-13 10:47:01 -0400

    Update predict_main.cpp

--- source/predict_main.cpp
+++ source/predict_main.cpp
@@ -46,16 +46,15 @@ int main(int argc, char *argv[])
 
 	Exondata exonin;
 	Exondata *indata = &exonin;
-	int predictor_size, sample_size, cluster_size, var_size, loci_size, bin_size;
+	int predictor_size, sample_size, cluster_size, var_size, loci_size, bin_size, DH_num1, DH_num2, DH_num3;
 
-	if (ReadPar(libfile, loci_size, predictor_size, cluster_size, bin_size, var_size))
+	if (ReadPar(libfile, loci_size, predictor_size, cluster_size, bin_size, var_size, DH_num1, DH_num2, DH_num3))
 	{
 		return 1;
 	}
 
 	double *quantile_in = new double[predictor_size];
 	int *cluster_idx = new int[predictor_size];
-	//int *TC_id = new int[predictor_size];
 	double *exon_mean = new double[predictor_size];
 	double *exon_sd = new double[predictor_size];
 	double *DNase_mean = new double[loci_size];
@@ -85,7 +84,57 @@ int main(int argc, char *argv[])
 		TC_id[j] = new char[30];
 	}
 
-	if (ReadinModel(libfile, quantile_in, exon_mean, exon_sd, coef, DNase_mean, DNase_sd, pre_idx, TC_id, cluster_idx, select_loci, predictor_size, var_size, loci_size))
+	//DH cluster model
+	double **dis_matrix = new double *[3];
+	for (int j = 0; j < 3; j++)
+	{
+		dis_matrix[j] = new double[loci_size]();
+	}
+
+	int **DH_cluster = new int *[3];
+	for (int j = 0; j < 3; j++)
+	{
+		DH_cluster[j] = new int[loci_size]();
+	}
+
+	double **DH_coef1 = new double *[var_size];
+	for (int j = 0; j < var_size; j++)
+	{
+		DH_coef1[j] = new double[DH_num1]();
+	}
+
+	double **DH_coef2 = new double *[var_size];
+	for (int j = 0; j < var_size; j++)
+	{
+		DH_coef2[j] = new double[DH_num2]();
+	}
+
+	double **DH_coef3 = new double *[var_size];
+	for (int j = 0; j < var_size; j++)
+	{
+		DH_coef3[j] = new double[DH_num3]();
+	}
+
+	int **DH_pre_idx1 = new int *[var_size];
+	for (int j = 0; j < var_size; j++)
+	{
+		DH_pre_idx1[j] = new int[DH_num1]();
+	}
+
+	int **DH_pre_idx2 = new int *[var_size];
+	for (int j = 0; j < var_size; j++)
+	{
+		DH_pre_idx2[j] = new int[DH_num2]();
+	}
+
+	int **DH_pre_idx3 = new int *[var_size];
+	for (int j = 0; j < var_size; j++)
+	{
+		DH_pre_idx3[j] = new int[DH_num3]();
+	}
+
+
+	if (ReadinModel(libfile, quantile_in, exon_mean, exon_sd, coef, DNase_mean, DNase_sd, pre_idx, TC_id, cluster_idx, select_loci, predictor_size, var_size, loci_size, dis_matrix, DH_cluster, DH_coef1, DH_coef2, DH_coef3, DH_pre_idx1, DH_pre_idx2, DH_pre_idx3, DH_num1, DH_num2, DH_num3))
 	{
 		return 1;
 	}
@@ -106,11 +155,18 @@ int main(int argc, char *argv[])
 	double ** data_norm = new double *[sample_size];
 	double ** data_mean = new double *[sample_size];
 	double ** output = new double *[sample_size];
+	double ** DH_pre1 = new double *[sample_size];
+	double ** DH_pre2 = new double *[sample_size];
+	double ** DH_pre3 = new double *[sample_size];
+
 	for (int i = 0; i < sample_size; i++)
 	{
 		data_norm[i] = new double[predictor_size]();
 		data_mean[i] = new double[cluster_size]();
 		output[i] = new double[loci_size]();
+		DH_pre1[i] = new double[DH_num1]();
+		DH_pre2[i] = new double[DH_num2]();
+		DH_pre3[i] = new double[DH_num3]();
 	}
 
 	for (int i = 0; i < sample_size; i++)
@@ -125,7 +181,18 @@ int main(int argc, char *argv[])
 
 	StandardizeRow(data_norm, exon_mean, exon_sd, predictor_size, sample_size);
 	ClusterMean(data_norm, data_mean, cluster_idx, predictor_size, cluster_size, sample_size); //get cluster mean;
-	Regression(data_mean, output, coef, pre_idx, var_size, loci_size, sample_size);
+	
+	//Locus level prediction
+	Regression(data_mean, output, coef, pre_idx, var_size, loci_size, sample_size); 
+	
+	//DH cluster level prediction
+	Regression(data_mean, DH_pre1, DH_coef1, DH_pre_idx1, var_size, DH_num1, sample_size);
+	Regression(data_mean, DH_pre2, DH_coef2, DH_pre_idx2, var_size, DH_num2, sample_size);
+	Regression(data_mean, DH_pre3, DH_coef3, DH_pre_idx3, var_size, DH_num3, sample_size);
+
+	//Combine result from locus level and DH cluster level
+	ModelAverage(output, DH_pre1, DH_pre2, DH_pre3, dis_matrix, DH_cluster, loci_size, sample_size);
+
 	StandardizeRow_r(output, DNase_mean, DNase_sd, loci_size, sample_size);
 
 	std::cout << "Writing output file..." << std::endl;
@@ -143,11 +210,25 @@ int main(int argc, char *argv[])
 		delete[] data_norm[i];
 		delete[] data_mean[i];
 		delete[] output[i];
+		delete[] DH_pre1[i];
+		delete[] DH_pre2[i];
+		delete[] DH_pre3[i];
 	}
 	for (int i = 0; i < var_size; i++)
 	{
 		delete[] coef[i];
 		delete[] pre_idx[i];
+		delete[] DH_coef1[i];
+		delete[] DH_coef2[i];
+		delete[] DH_coef3[i];
+		delete[] DH_pre_idx1[i];
+		delete[] DH_pre_idx2[i];
+		delete[] DH_pre_idx3[i];
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		delete[] dis_matrix[i];
+		delete[] DH_cluster[i];
 	}
 	for (int i = 0; i < predictor_size; i++)
 	{
@@ -160,9 +241,20 @@ int main(int argc, char *argv[])
 	delete[] data_norm;
 	delete[] data_mean;
 	delete[] output;
+	delete[] DH_pre1;
+	delete[] DH_pre2;
+	delete[] DH_pre3;
 	delete[] coef;
 	delete[] pre_idx;
+	delete[] DH_coef1;
+	delete[] DH_coef2;
+	delete[] DH_coef3;
+	delete[] DH_pre_idx1;
+	delete[] DH_pre_idx2;
+	delete[] DH_pre_idx3;
 	delete[] TC_id;
 	delete[] select_loci;
+	delete[] dis_matrix;
+	delete[] DH_cluster;
 	return 0;
 }
