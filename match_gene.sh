e0831303fb7b699b7453d59f229407fc9ca56086
Weiqiang Zhou <kenzhou86@gmail.com>
2015-11-05 11:15:02 -0500

    Update match_gene.sh

--- match_gene.sh
+++ match_gene.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ##Usage: bash match_gene.sh input_file ref_gene_file output_file
+##In input_file, column 4 is gene_id and column 10 is FPKM.
 
 awk -v OFS='\t' 'NR==FNR {h[$4] = $10; next} {print $1,h[$1]}' $1 $2 > $3.temp
 echo gene_id$'\t'FPKM | cat - $3.temp > $3
