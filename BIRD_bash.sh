64a70d74afaf3d5f2dcb1318f504a5d64d0527bd
Weiqiang Zhou <kenzhou86@gmail.com>
2018-08-29 16:03:04 -0400

    Add files via upload

--- BIRD_bash.sh
+++ BIRD_bash.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
-##Usage: bash BIRD_bash.sh BIRD_directory input_file output_name partition_size wig_mode
-if [ $# -ne 5 ]
-then echo "Usage: bash BIRD_bash.sh BIRD_directory input_file output_name partition_size wig_mode"
+##Usage: bash BIRD_bash.sh BIRD_directory model_file input_file output_name partition_size wig_mode
+if [ $# -ne 6 ]
+then echo "Usage: bash BIRD_bash.sh BIRD_directory model_file input_file output_name partition_size wig_mode"
      echo "Example:"
-     echo "WIG output: bash BIRD_bash.sh BIRD_v1.0.1 input.txt output_name 100 1"
-     echo "Standard output: bash BIRD_bash.sh BIRD_v1.0.1 input.txt output_name 100 0"
+     echo "WIG output: bash BIRD_bash.sh path_to_BIRD path_to_BIRD/model/model_file.bin input.txt output_name 100 1"
+     echo "Standard output: bash BIRD_bash.sh path_to_BIRD path_to_BIRD/model/model_file.bin input.txt output_name 100 0"
 else
 
 if [ ! -d "BIRD_tmp" ]
 then mkdir ./BIRD_tmp
 fi
 
-Rscript $1/R_script/split_input.r $2 $4
+Rscript $1/R_script/split_input.r $3 $5
 
 if [ -f "./BIRD_tmp/BIRD_input_part1" ]
 then
 
-if [ $5 -eq "1" ]
+if [ $6 -eq "1" ]
 then
 for file in ./BIRD_tmp/BIRD_input_part*
 do
     echo "Processing ${file}"
-    $1/BIRD_predict -b $1/model/model_file.bin -i ${file} -o $3 -w
+    $1/BIRD_predict -b $2 -i ${file} -o $4 -w
 done
 else
 for file in ./BIRD_tmp/BIRD_input_part*
 do
     echo "Processing ${file}"
     part_num=${file##*BIRD_input_}
-    $1/BIRD_predict -b $1/model/model_file.bin -i ${file} -o $3.${part_num}
+    $1/BIRD_predict -b $2 -i ${file} -o $4_${part_num}.txt
 done
 fi
 
