d4a70a27223e8261130131ec2e4055aaace5484c
Weiqiang Zhou <kenzhou86@gmail.com>
2019-07-08 11:36:55 -0400

    Update split_input.r

--- R_script/split_input.r
+++ R_script/split_input.r
@@ -1,4 +1,6 @@
 ##Usage: Rscript split_input.r input_file partition_size##
+library(data.table)
+
 input_param <- commandArgs(TRUE)
 
 if(length(input_param)!=2){
@@ -8,7 +10,7 @@ q(save="no")
 }
 
 sep_size <- as.numeric(input_param[2])
-data_in <- read.table(file=input_param[1],comment.char="",check.names=FALSE)
+data_in <- data.frame(fread(file=input_param[1]))
 sample_size <- dim(data_in)[2] - 1
 
 if(sample_size<=sep_size){
@@ -20,7 +22,7 @@ cut_size <- floor(sample_size/sep_size)
 sub_idx <- cut(seq(1,sample_size),breaks=cut_size,label=FALSE)
 
 for(i in 1:cut_size){
-write.table(data.frame(data_in[,1],data_in[,which(sub_idx==i)+1]),file=paste0("./BIRD_tmp/BIRD_input_part",i),row.names=FALSE,col.names=FALSE,sep="\t",quote=FALSE)
+write.table(data.frame(gene_id=data_in[,1],data_in[,which(sub_idx==i)+1]),file=paste0("./BIRD_tmp/BIRD_input_part",i),row.names=FALSE,col.names=TRUE,sep="\t",quote=FALSE)
 }
 
 q(save="no")
